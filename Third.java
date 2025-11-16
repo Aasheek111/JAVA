8dca50cee9b3d59f19eecdca900c1bda3c35e848
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-16 08:59:42 +0545

    multiply

--- Third.java
+++ Third.java
@@ -1,3 +1,14 @@
 public class Third {
+
+    public static void main (){
+        int i;
+        int number=2;
+
+
+        for (i=1;i<+10;i++){
+            System.out.println(i+" * "+number+" = "+i*number);
+        }
+    }
+
     
 }
