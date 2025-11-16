c7ceec2bdc54eda685cf53f3c414caf8af7011c1
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-16 08:51:07 +0545

    initial

---
+++ First.java
@@ -0,0 +1,7 @@
+// Simple Java Program
+public class First{
+    public static void main(String[] args) {
+        System.out.println("Hello, Aashik! Welcome to Java Programming!");
+    }
+}
+
---
+++ Second.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Second {
+
+    public static void main(){
+
+        scanner =new Scanner(System.in);
+
+
+
+    }
+
+    
+    
+}
