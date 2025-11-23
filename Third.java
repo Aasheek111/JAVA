e1f939053287b075067a90b37827ac61929a03a5
Aashik Gautam <technicalaashik111@gmail.com>
2025-11-23 08:40:24 +0545

    adding new files

--- Third.java
+++ Third.java
@@ -1,14 +1,12 @@
 public class Third {
 
-    public static void main (){
+    public static void main() {
         int i;
-        int number=2;
+        int number = 2;
 
-
-        for (i=1;i<+10;i++){
-            System.out.println(i+" * "+number+" = "+i*number);
+        for (i = 1; i < +10; i++) {
+            System.out.println(i + " * " + number + " = " + i * number);
         }
     }
 
-    
 }
